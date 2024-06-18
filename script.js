dec52e3e2ec8d0fe13242ceaa79da35654208e90
Ankit Agarwal <152173656+agankit2004@users.noreply.github.com>
2024-06-18 20:02:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function search() {
+    let input = document.getElementById('input').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('options');
+    let physics = document.getElementById('Physics')
+    let chemistry = document.getElementById('Chemistry')
+    let maths = document.getElementById('Maths')
+  
+    for (i = 0; i < x.length; i++) {
+      if (!x[0].innerHTML.toLowerCase().includes(input)) {
+            physics.style.display="none"
+      }
+      else {
+        physics.style.display=""
+      }
+      if(!x[1].innerHTML.toLowerCase().includes(input)) {
+        chemistry.style.display="none"
+     }
+     else {
+
+        chemistry.style.display=""
+
+      }
+      if(!x[2].innerHTML.toLowerCase().includes(input)) {
+        maths.style.display="none"
+     }
+      else {
+
+        maths.style.display=""
+      }
+    }
+  }
